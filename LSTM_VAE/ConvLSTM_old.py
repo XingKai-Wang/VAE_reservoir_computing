a5cb509fa05092f04b0568d3cd86fde7c45cb114
Xingkai <xk.wang_gakki@outlook.com>
2021-06-05 21:25:27 +0200

    old version of convlstm

---
+++ LSTM_VAE/ConvLSTM_old.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+from LSTM_VAE.MovingMNIST_dataset import *
+
+
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_channels, hidden_channels, kernel_size):
+        super(ConvLSTMCell, self).__init__()
+        self.input_channels = input_channels
+        self.hidden_channels = hidden_channels
+        self.kernel_size = kernel_size
+
+        self.padding = int((kernel_size - 1) / 2)
+
+        # input cell
+        self.wxi = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.whi = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+
+        # forget cell
+        self.wxf = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.whf = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+
+        # memory cell
+        self.wxc = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.whc = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+
+        # output cell
+        self.wxo = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=True)
+        self.who = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=False)
+
+        self.wci = None
+        self.wcf = None
+        self.wco = None
+
+    def forward(self, x, h, c):
+        ci = torch.sigmoid(self.wxi(x) + self.whi(h) + self.wci * c)
+        cf = torch.sigmoid(self.wxf(x) + self.whf(h) + self.wcf * c)
+        cc = cf * c + ci * torch.tanh(self.wxc(x) + self.whc(h))
+        co = torch.sigmoid(self.wxo(x) + self.who(h) + self.wco * cc)
+        ch = co * torch.tanh(cc)
+
+        return ch, cc
+
+    def init_hidden(self, batch_size, hidden, shape):
+        if self.wci is None:
+            self.wci = nn.Parameter(torch.zeros(1, hidden, shape[0], shape[1])).cuda()
+            self.wcf = nn.Parameter(torch.zeros(1, hidden, shape[0], shape[1])).cuda()
+            self.wco = nn.Parameter(torch.zeros(1, hidden, shape[0], shape[1])).cuda()
+        else:
+            assert shape[0] == self.wci.size()[2], 'Input width mismatched'
+            assert shape[1] == self.wci.size()[3], 'Input height mismatched'
+
+        return (Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda(),
+                Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda())
+
+
+class ConvLSTM(nn.Module):
+    def __init__(self, input_channels, hidden_channels, kernel_size):
+        super(ConvLSTM, self).__init__()
+        self.input_channels = [input_channels] + hidden_channels
+        self.hidden_channels = hidden_channels
+        self.num_layers = len(hidden_channels)
+        self.kernel_size = kernel_size
+        self._all_layers = []
+
+        for i in range(self.num_layers):
+            name = 'cell{}'.format(i)
+            cell = ConvLSTMCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size)
+            setattr(self, name, cell)
+            self._all_layers.append(cell)
+
+    def forward(self, input):
+        internal_state = []
+        outputs = []
+        for seq_index in range(input.size(1)):
+            x = input[:, seq_index, :, :]
+            for i in range(self.num_layers):
+                # intialize all cells in first step
+                name = 'cell{}'.format(i)
+                if seq_index == 0:
+                    batch_size, _, h, w = x.size()
+                    (h, c) = getattr(self, name).init_hidden(batch_size, self.hidden_channels[i], (h, w))
+
+                    internal_state.append((h, c))
+                # do forward
+                (h, c) = internal_state[i]
+                cur_x = x if i == 0 else internal_state[i-1][0]
+                x, new_c = getattr(self, name)(cur_x, h, c)
+                internal_state[i] = (x, new_c)
+
+            outputs.append(x)
+
+        return reversed(torch.stack(outputs, dim=1))
